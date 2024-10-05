1e45953ead3c5411a4a33eb93ee8bb2da9ec78b4
shams512 <shamssali729@outlook.com>
2024-10-05 15:04:57 +0500

    Adding new Tag like App

--- VirtualRoom-js/src/main.jsx
+++ VirtualRoom-js/src/main.jsx
@@ -2,10 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import Tag from './tag.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <Tag />
   </StrictMode>,
 )
 
---
+++ VirtualRoom-js/src/tag.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function Tag()
+{  const [count, setCount] = useState(0);
+    return
+    (
+        <>
+            <h1> Shams Ali created a tag </h1>
+        
+        </>
+    );
+}
+
+export default Tag;
