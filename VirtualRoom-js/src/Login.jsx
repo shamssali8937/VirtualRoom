632fffb5a3093a8a6a766fc60183d5810c843649
shams512 <shamssali729@outlook.com>
2024-10-05 18:03:39 +0500

    imported images to login.jsx

--- VirtualRoom-js/src/Login.jsx
+++ VirtualRoom-js/src/Login.jsx
@@ -1,7 +1,13 @@
 import React from "react";
-import 'Login.css';
+import './Login.css';
+import user from "./assets/Images/person.png"
+import emailicon from './assets/Images/email.png'
+import passicon from './assets/Images/password.png'
+
 
 function Login()
 {
     return(<><h1>shas</h1></>);
 }
+
+export default Login
