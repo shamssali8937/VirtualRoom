ac5158145be6f55a81dee6f2e4d346d1ec1437c2
shams512 <shamssali729@outlook.com>
2024-10-05 18:18:02 +0500

    Added Inputs for signup

--- VirtualRoom-js/src/Login.jsx
+++ VirtualRoom-js/src/Login.jsx
@@ -7,7 +7,26 @@ import passicon from './assets/Images/password.png'
 
 function Login()
 {
-    return(<><h1>shas</h1></>);
+    return(<>
+    <div className="container">
+        <div className="header">
+            <div className="text">Signup</div>
+            <div className="underline"></div>
+        </div>
+        <div className="inputs">
+            <div className="input">
+                <img src={user} alt="" />
+                <input type="text" name="" id="" />
+                <img src={user} alt="" />
+                <input type="text" name="" id="" />
+                <img src={emailicon} alt="" />
+                <input type="email" name="" id="" />
+                <img src={passicon} alt="" />
+                <input type="password" name="" id="" />
+            </div>
+        </div>
+    </div>
+    </>);
 }
 
 export default Login
