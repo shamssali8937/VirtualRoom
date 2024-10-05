9807e3e21f3700878becf32d19c0676ca54fded4
shams512 <shamssali729@outlook.com>
2024-10-05 23:11:57 +0500

    created login and reguster page

--- VirtualRoom-js/src/Login.jsx
+++ VirtualRoom-js/src/Login.jsx
@@ -16,6 +16,13 @@ function Login()
         password:""
     });
 
+    const handlechange=(event)=>{
+        const{name,value}=event.target;
+        setdata({
+            ...data,
+            [name]:value
+        })
+    }
     const handlesubmit=(event)=>{
           event.preventDefault();
           if(action==="Signup")
@@ -27,9 +34,31 @@ function Login()
             else
             {
                 alert("Registered");
+                clearfields(); 
+            }
+          }
+          else
+          {
+            if(!data.email||!data.password)
+            {
+                alert("Please enter the credentials");
+            }
+            else
+            {
+                alert("LogedIn");
+                clearfields(); 
             }
           }
     }
+
+    const clearfields = () => {
+        setdata({
+            name: "",
+            username: "",
+            email: "",
+            password: ""
+        });
+    };
 
     return(<>
     <div className="container">        
@@ -37,48 +66,48 @@ function Login()
             <div className="text">{action}</div>
             <div className="underline"></div>
         </div>
-        <div className="inputs">
+        <form className="inputs" onSubmit={handlesubmit}>
         {action==="Login"?<div></div>:
         <>
             <div className="input">              
                 <img src={user} alt="" />
-                <input type="text" name="" id="" placeholder="Name" required/>
+                <input type="text" name="name" id="" placeholder="Name" value={data.name} onChange={handlechange} required/>
               </div> 
               <div className="input">
                 <img src={user} alt="" />
-                <input type="text" name="" id="" placeholder="Username" required/>
+                <input type="text" name="username" id="" placeholder="Username" value={data.username} onChange={handlechange} required/>
               </div>       
         </>
         }
               <div className="input">
                 <img src={emailicon} alt="" />
-                <input type="email" name="" id="" placeholder="Email" required/>
+                <input type="email" name="email" id="" placeholder="Email" value={data.email} onChange={handlechange} required/>
               </div>
               <div className="input">
                 <img src={passicon} alt="" />
-                <input type="password" name="" id="" placeholder="Password" required/>
+                <input type="password" name="password" id="" placeholder="Password" value={data.password} onChange={handlechange} required/>
               
-            </div>           
-        </div>
+            </div>                   
         <div className="submit-container">
             <button className={action==="Login"?"submit gray":"submit"} onClick={()=>{
                 setaction("Signup");
-                if(action==="Signup")
-                {
-                    onclick=()=>{
-                        if(data.email&&data.name&&data.username&&data.password===null)
-                        {
-                            alert("enter credntial");
-                        }
+                // if(action==="Signup")
+                // {
+                //     onclick=()=>{
+                //         if(data.email&&data.name&&data.username&&data.password===null)
+                //         {
+                //             alert("enter credntial");
+                //         }
                         
-                    }
-                }
+                //     }
+                // }
             }}>Sign up</button>
-            <button type="submit" formMethod="post" className={action==="Signup"?"submit gray":"submit"} onClick={()=>{
+            <button type="submit"  className={action==="Signup"?"submit gray":"submit"} onClick={()=>{
                 setaction("Login");
                 
             }}>Login</button>
         </div>
+    </form>
     </div>
     </>);
 }
