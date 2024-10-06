1510096334c9df46d59576543779d4d4d16ec8af
shams512 <shamssali729@outlook.com>
2024-10-06 16:18:09 +0500

    Successfully created routes

--- VirtualRoom-js/src/Login2.jsx
+++ VirtualRoom-js/src/Login2.jsx
@@ -1,32 +1,22 @@
 import React from "react";
-import { useState } from 'react'
 import { Link } from 'react-router-dom';
+import { useState } from 'react'
 import './Signup.css';
-import user from "./assets/Images/person.png"
 import emailicon from './assets/Images/email.png'
 import passicon from './assets/Images/password.png'
 
-function Signup()
+
+function Login()
 {
-    
     return(
     <>
-
     <div className="container">
         <div className="header">
-            <div className="text">Signup</div>
+            <div className="text">Login</div>
              <div className="underline"></div>
         </div>
      
-            <form action="Post" className="inputs">
-                <div className="input">
-                <img src={user} alt="user" />
-                <input type="text" placeholder="Name" required/>
-                </div>
-                <div className="input">
-                <img src={user} alt="user" />
-                <input type="text" placeholder="Username" required/>
-                </div>
+            <form action="Post" className="inputs">               
                 <div className="input">
                 <img src={emailicon} alt="email" />
                 <input type="email" placeholder="Email" required/>
@@ -36,17 +26,18 @@ function Signup()
                 <input type="password" placeholder="Password" required/>
                 </div>
                 <div className="login">
-                    {/* <a href="https://www.w3schools.com/react/react_forms.asp" target="blank">Already Have Account?</a> */}
-                    <Link to="/login">Already Have Account?</Link>
+                    <a href="" target="blank">Forgot Password</a>
+                </div>
+                <div className="login">
+                    <Link to="/signup">Register?</Link>
                 </div>
                 <div className="submit-container">
-                    <button href="" className="submit" type="submit">Signup</button>
+                    <button href="" className="submit" type="submit">Login</button>
                 </div>
             </form>
     </div>
 
-
     </>);
 }
 
-export default Signup;
+export default Login;
--- VirtualRoom-js/src/main.jsx
+++ VirtualRoom-js/src/main.jsx
@@ -1,18 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter as Router } from 'react-router-dom'
 import App from './App.jsx'
  import './index.css'
 import Tag from './Tag.jsx'
-import Login from './Login.jsx'
+// import Login from './Login.jsx'
 import Signup from './Signup.jsx'
+import Login from './Login2.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
   <>
-      {/* <App />
-      <Tag /> */}
+         <Router>
+             <App />
+         </Router>
+    
+      {/* <Tag /> */}
       {/* <Login /> */}
-      <Signup />
+      {/* <Signup /> */}
+      {/* <Login /> */}
   </>
     
     
